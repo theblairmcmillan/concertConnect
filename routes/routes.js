a7ce534a07d40472a5a7fbf59802dafb122c0638
BlairMcMillan <theblairmcmillan@gmail.com>
2016-06-18 14:07:59 -0500

    getting forms in frontend

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 // DEPENDENCIES 
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 
 //MODELS 
@@ -9,10 +10,42 @@ var artists = require('./artists.routes');
 var hosts = require('./hosts.routes');
 
 
+// // INDEX 
+// router.get('/', function(req, res) {
+// 	res.render('index'); 
+// });
+
+
+// // LOGIN 
+// router.get('/api/login', function(req, res) {
+//     res.render('login', { message: req.flash('loginMessage') }); 
+// });
+
+// // SIGNUP 
+// router.get('/api/signup', function(req, res) {
+//     res.render('signup.ejs', { message: req.flash('signupMessage') });
+// });
+
+
+// LOGOUT 
+router.get('/api/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+
 //ROUTES 
 router.use(artists);
 router.use(hosts);
 
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+	// if user is authenticated in the session, carry on 
+	if (req.isAuthenticated())
+		return next();
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
 
 
 //RETURN ROUTER//
