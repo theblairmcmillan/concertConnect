01673d98f7dc01496ccc85d69e2687c4a4819f55
BlairMcMillan <theblairmcmillan@gmail.com>
2016-06-22 19:12:04 -0500

    got bandcamp apearing

--- public/app/artist/artistViewController.js
+++ public/app/artist/artistViewController.js
@@ -3,8 +3,10 @@ app.controller("artistViewController", ["$scope", "$location","$http", "$routePa
 function ($scope, $location, $http, $routeParams, $timeout, Upload, userFactory) {
 
 	$scope.currentUser = {};
+	$scope.bandcamp = "";
 	$timeout(function() {
-		$scope.currentUser = userFactory.getUserData()
+		$scope.currentUser = userFactory.getUserData();
+		$("#bandcamp").html($scope.currentUser.artist.bandcamp)
 		console.log(">>>>", $scope.currentUser);
 	}, 2000);
 
@@ -59,6 +61,7 @@ function ($scope, $location, $http, $routeParams, $timeout, Upload, userFactory)
 		})
 	};
 
+	// UPDATE ABOUT SECTION //
 	$scope.updateAbout = function(updateAboutField){
 		console.log("got inside update about");
 		$http({
@@ -73,7 +76,21 @@ function ($scope, $location, $http, $routeParams, $timeout, Upload, userFactory)
 		})
 	};
 
-
+	// UPDATE BANDCAMP // 
+	$scope.bandcampUpload = function(bandcampString){
+		console.log("got inside bandcamp function");
+		$http({
+			method: 'POST',
+			url: `/api/artists/${$scope.currentUser.artist._id}`,
+			data: {
+				bandcamp: bandcampString
+			}
+		}).success(function(data){
+			console.log(data);
+			$("#bandcamp").html(data.bandcamp)
+			userFactory.setUserData($scope.currentUser._id);
+		})
+	};
 
 
 
